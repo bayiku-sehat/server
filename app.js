651c086214e0d6fa6e2983d6f43e2edde584e727
Bimo dwien Prabowo <bimodwienprabowo@Bimos-MacBook-Air.local>
2020-12-12 23:09:31 +0700

    update: tambah errorhandler di appjs

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
 const router = require('./routes/index')
+const errorHandler = require('./middlewares/errHandler')
 
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(router)
+app.use(errorHandler)
 
 
 app.listen(PORT, () => {
