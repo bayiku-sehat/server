e8a4622846d4bce3b8c7811302c41d3816f7cc0e
Reynaldi Riva <reynaldi.riva@gmail.com>
2020-12-15 14:05:16 +0700

    update seeders

---
+++ data/bayiuser.js
@@ -0,0 +1,70 @@
+module.exports = [
+  {
+    BayiId: 1,
+    UserId: 1,
+  },
+  {
+    BayiId: 1,
+    UserId: 7,
+  },
+  {
+    BayiId: 1,
+    UserId: 2,
+  },
+  {
+    BayiId: 1,
+    UserId: 3,
+  },
+  {
+    BayiId: 2,
+    UserId: 2,
+  },
+  {
+    BayiId: 2,
+    UserId: 3,
+  },
+  {
+    BayiId: 3,
+    UserId: 2,
+  },
+  {
+    BayiId: 3,
+    UserId: 3,
+  },
+  {
+    BayiId: 4,
+    UserId: 2,
+  },
+  {
+    BayiId: 4,
+    UserId: 3,
+  },
+  {
+    BayiId: 5,
+    UserId: 2,
+  },
+  {
+    BayiId: 5,
+    UserId: 3,
+  },
+  {
+    BayiId: 2,
+    UserId: 4,
+  },
+  {
+    BayiId: 3,
+    UserId: 5,
+  },
+  {
+    BayiId: 4,
+    UserId: 6,
+  },
+  {
+    BayiId: 5,
+    UserId: 1,
+  },
+  {
+    BayiId: 5,
+    UserId: 1,
+  },
+]
